a247163c635982bed5d19a17c1d1d91fad1cfea1
asit singh <asitsingh0@gmail.com>
2020-09-19 04:32:59 +0530

    Updates

---
+++ precache-manifest.a3a0db09b5dccde9e291a81be800e01c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f182037e7a8952b1db0657464e3c581",
+    "url": "/reactblog/index.html"
+  },
+  {
+    "revision": "630c22ece596efb0ac27",
+    "url": "/reactblog/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "07c5aa44a936f73092a6",
+    "url": "/reactblog/static/css/main.0449d095.chunk.css"
+  },
+  {
+    "revision": "630c22ece596efb0ac27",
+    "url": "/reactblog/static/js/2.6c0c2c5f.chunk.js"
+  },
+  {
+    "revision": "c3af2aa7bbf7561b9d2cc21ffee3ea4a",
+    "url": "/reactblog/static/js/2.6c0c2c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c5aa44a936f73092a6",
+    "url": "/reactblog/static/js/main.8dc4c44f.chunk.js"
+  },
+  {
+    "revision": "14fc890242d0b51528db",
+    "url": "/reactblog/static/js/runtime-main.686638e4.js"
+  },
+  {
+    "revision": "6798322b95ec9fbf407b3364fec44b12",
+    "url": "/reactblog/static/media/20859.6798322b.jpg"
+  },
+  {
+    "revision": "56fd849e9c022339518b9f9cbb4afba5",
+    "url": "/reactblog/static/media/2170435.56fd849e.jpg"
+  },
+  {
+    "revision": "515afb9023cf439d1bc168fea08ec7f7",
+    "url": "/reactblog/static/media/631.515afb90.jpg"
+  },
+  {
+    "revision": "effcd6560b0e59b30c54f5192c47ded6",
+    "url": "/reactblog/static/media/632.effcd656.jpg"
+  },
+  {
+    "revision": "a75f74ee7f8a0f1241ff5257c542bbb2",
+    "url": "/reactblog/static/media/6547.a75f74ee.jpg"
+  },
+  {
+    "revision": "d015cc2938876d58adee564a3a042836",
+    "url": "/reactblog/static/media/Busines.d015cc29.jpg"
+  },
+  {
+    "revision": "a29d676cf1548fd81d2116c417a5437e",
+    "url": "/reactblog/static/media/accounting.a29d676c.jpg"
+  },
+  {
+    "revision": "ccb0990b1ad80317761ba1462d42e0a6",
+    "url": "/reactblog/static/media/art.ccb0990b.jpg"
+  },
+  {
+    "revision": "6380d37f9983de8d2c70d56f28aa0818",
+    "url": "/reactblog/static/media/consultancy1.6380d37f.png"
+  },
+  {
+    "revision": "972ed0fc1aa281508897b200f9d70a62",
+    "url": "/reactblog/static/media/english.972ed0fc.jpg"
+  },
+  {
+    "revision": "35d89a8f1c005a15f518468c24497bb4",
+    "url": "/reactblog/static/media/h&f.35d89a8f.jpg"
+  },
+  {
+    "revision": "383fc7767386d08b6b5042bf5f7ec89a",
+    "url": "/reactblog/static/media/logo.383fc776.png"
+  },
+  {
+    "revision": "aec05d4156b35af41660e3a479288bfc",
+    "url": "/reactblog/static/media/map.aec05d41.jpg"
+  },
+  {
+    "revision": "b26d89f637aaa6761adeb684f6b0cd9f",
+    "url": "/reactblog/static/media/math.b26d89f6.jpg"
+  },
+  {
+    "revision": "0073f2ea44763773db7e281cb9b10385",
+    "url": "/reactblog/static/media/meeting-3.0073f2ea.png"
+  },
+  {
+    "revision": "9e93c20a471549a421a1bc77beb2b920",
+    "url": "/reactblog/static/media/photography.9e93c20a.jpg"
+  },
+  {
+    "revision": "f85aa714ab3026beaf660938830edb36",
+    "url": "/reactblog/static/media/science.f85aa714.jpg"
+  }
+]);
